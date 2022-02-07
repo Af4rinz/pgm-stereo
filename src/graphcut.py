22e59112ca0df3a05e2a6d2889b5616261dc0046
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2022-02-07 11:52:11 +0330

    more samples!

--- src/graphcut.py
+++ src/graphcut.py
@@ -6,24 +6,23 @@ import maxflow
 from utils import pair2grey
 
 class Graph_Cut_Solver:
+    # pixel is occluded
     OCCLUDED_LABEL = 1
+    # df(p) = a
+    VAR_ALPHA = -1
+    # df(p) occluded or not in the right image
+    VAR_ABSENT = -2
     MAX_ITER = 4
 
     def __init__(self, left, right, 
-    search_depth=50, max_steps=-1, occ_cost=-1, 
-    smooth_cost_lo=-1, smooth_cost_hi=-1, smooth_thresh=10):
+    search_depth=50, max_steps=-1,occ_cost=-1,
+    smooth_cost=-1, smooth_thresh=10):
         self.left, self.right = pair2grey(left, right)
         self.img_shape = self.left.shape
         self.img_indices = np.indices(self.img_shape)
         self.img_size = self.left.size
 
         self.search_depth = search_depth
-        self.occ_cost = occ_cost
-        self.smooth_cost_lo = smooth_cost_lo if smooth_cost_lo > 0 else occ_cost * 0.2
-        self.smooth_cost_hi = smooth_cost_hi if smooth_cost_hi > 0 else occ_cost * 3
-        self.smooth_thresh = smooth_thresh
-        self.energy = float('inf')
-
         self.max_steps = max_steps if max_steps > 0 else search_depth
         self.search_interval = (search_depth // self.max_steps) + bool(search_depth % self.max_steps)
         self.search_steps = -1 * np.arange(0, search_depth + 1, self.search_interval)[::-1]
@@ -31,7 +30,12 @@ class Graph_Cut_Solver:
         rank[np.argsort(self.search_steps)] = np.arange(len(self.search_steps))
         self.label_rank = dict(zip(self.search_steps, rank))
 
-        # construct neighbours and edges
+        self.occ_cost = occ_cost
+        self.smooth_cost = smooth_cost if smooth_cost > 0 else  occ_cost * 0.2
+        self.smooth_thresh = smooth_thresh
+        self.energy = float('inf')
+
+        # construct the network neighbours
         indices = np.indices(self.img_shape)
         n1p = indices[:, 1: , :].reshape(2, -1)
         n1q = n1p + [[-1] , [0]]
@@ -42,7 +46,7 @@ class Graph_Cut_Solver:
 
         idx_p , idx_q = self.neighbours
         left_diff = self.left[list(idx_p)] - self.left[list(idx_q)]
-        self.is_left_under = np.abs(left_diff) < self.smooth_thresh
+        self.is_left_behind = np.abs(left_diff) < self.smooth_thresh
 
     def solve(self):
         self.labels = np.full(self.img_shape, self.OCCLUDED_LABEL, dtype=np.int)
@@ -51,7 +55,7 @@ class Graph_Cut_Solver:
         for i in range(self.MAX_ITER):
             start = time.time()
             label_order = np.random.permutation(self.search_steps)
-            print(label_order)
+            print(f'Label order in iteration {i}: {label_order}')
             for label in label_order:
                 label_idx = self.label_rank[label]
                 if label_done[label_idx]:
@@ -62,6 +66,7 @@ class Graph_Cut_Solver:
                 label_done[label_idx] = True
                 # all labels are fully expanded
             print('Iteration {} time: {}'.format(i, time.time() - start))
+            print('----------------------------------------')
             if label_done.all():
                 break
         return -1 * self.labels
@@ -70,9 +75,9 @@ class Graph_Cut_Solver:
         is_expanded = False
         G = maxflow.Graph[int](2 * self.img_size, 12 * self.img_size)
 
-        self.add_E_data_occ(G, label)
-        self.add_E_smooth(G, label)
-        self.add_E_unique(G, label)
+        self.calculate_E_data_occ(G, label)
+        self.calculate_E_smooth(G, label)
+        self.calculate_E_unique(G, label)
         E = G.maxflow() + self.E_data_occ
 
         if E < self.energy:
@@ -82,28 +87,29 @@ class Graph_Cut_Solver:
         return is_expanded
     
 
-    def add_E_data_occ(self, G, label):
+    def calculate_E_data_occ(self, G, label):
         y_idx, x_idx = self.img_indices
-        is_label = self.labels == label
+        is_label = (self.labels == label)
         is_occluded = self.labels == self.OCCLUDED_LABEL
         idx_shifted = np.where(is_occluded, x_idx, x_idx + self.labels)
         # ACTIVE NODES
         active_ssd = np.square(self.left - self.right[y_idx, idx_shifted]) - self.occ_cost
         active_ssd[is_label | is_occluded] =  -self.occ_cost - 1
         active_nodes = np.zeros(self.img_shape, dtype=np.int)
-        active_nodes[is_label] = -1
-        active_nodes[is_occluded] = -2
+        active_nodes[is_label] = VAR_ALPHA
+        active_nodes[is_occluded] = VAR_ABSENT
         is_node_active = np.logical_not(is_label | is_occluded)
 
         E_data_occ = active_ssd[is_label].sum()
+        
         # LABEL NODES
         is_occluded = np.logical_not(self._is_in_img(x_idx + label))
         idx_shifted = np.where(is_occluded, x_idx, x_idx + label)
         label_ssd = np.square(self.left - self.right[y_idx, idx_shifted]) - self.occ_cost
         label_ssd[is_label | is_occluded] =  -self.occ_cost - 1
         label_nodes = np.zeros(self.img_shape, dtype=np.int)
-        label_nodes[is_label] = -1
-        label_nodes[is_occluded] = -2
+        label_nodes[is_label] = VAR_ALPHA
+        label_nodes[is_occluded] = VAR_ABSENT
         is_node_label = np.logical_not(is_label | is_occluded)
 
         node_count = is_node_label.sum() + is_node_active.sum()
@@ -129,7 +135,7 @@ class Graph_Cut_Solver:
         self.label_nodes = label_nodes
         self.E_data_occ = E_data_occ
     
-    def add_E_smooth(self, G, label):
+    def calculate_E_smooth(self, G, label):
         p_labels, q_labels = self.labels[self.neighbours_roll]
         penalty_label = self.get_smooth_penalty(label)
         penalty_active_p = self.get_smooth_penalty(p_labels)
@@ -145,13 +151,13 @@ class Graph_Cut_Solver:
             node_active_p, node_active_q = self.active_nodes[y_idx, x_idx]
             is_p_active, is_q_active = self.is_node_active[y_idx, x_idx]
 
-            if node_label_p != -2 and node_label_q != -2:
+            if node_label_p != VAR_ABSENT and node_label_q != VAR_ABSENT:
                 penalty = penalty_label[nidx]
-                if node_label_p != -1 and node_label_q != -1:
+                if node_label_p != VAR_ALPHA and node_label_q != VAR_ALPHA:
                     self.add_smooth_weights(G, node_label_p, node_label_q, 0, penalty, penalty, 0)
-                elif node_label_p != -1:
+                elif node_label_p != VAR_ALPHA:
                     G.add_tedge(node_label_p, 0, penalty)
-                elif node_label_q != -1:
+                elif node_label_q != VAR_ALPHA:
                     G.add_tedge(node_label_q, 0, penalty)
             penalty_p, penalty_q = penalty_active_p[nidx], penalty_active_q[nidx]
 
@@ -165,7 +171,7 @@ class Graph_Cut_Solver:
             if is_q_active and is_p_in_img[nidx]:
                 G.add_tedge(node_active_q, 0, penalty_q)
     
-    def add_E_unique(self, G, label):
+    def calculate_E_unique(self, G, label):
         h, w = self.img_shape
         y_idx, x_idx = self.img_indices
         idx_shifted = x_idx + self.labels - label
@@ -176,25 +182,26 @@ class Graph_Cut_Solver:
         ban_active = self.active_nodes[banned]
 
         self.add_unique_weights(G, ban_label, ban_active)
-        is_node_label = self.label_nodes != -2
+        is_node_label = self.label_nodes != VAR_ABSENT
         banned = self.is_node_active & is_node_label
         self.add_unique_weights(G, self.label_nodes[banned], self.active_nodes[banned])
 
     def add_unique_weights(self, G, sinks, sources):
         for i in range(sources.size):
-            G.add_edge(sources[i], sinks[i], int('854775807'), 0)
+            # really big number (maxsize seems to cause problems)
+            G.add_edge(sources[i], sinks[i], int('864775807'), 0)
     
     def get_smooth_penalty(self, labels):
         p_idx, q_idx = self.neighbours
         if type(labels) is np.ndarray:
-            labels = labels[self.is_left_under]
-        smoothness = np.full(p_idx.shape[1], self.smooth_cost_lo, dtype=np.float)
-        p_idx_shifted, is_p_in_img = self._shift(p_idx[:, self.is_left_under], labels)
-        q_idx_shifted, is_q_in_img = self._shift(q_idx[:, self.is_left_under], labels)
+            labels = labels[self.is_left_behind]
+        smoothness = np.full(p_idx.shape[1], self.smooth_cost, dtype=np.float)
+        p_idx_shifted, is_p_in_img = self._shift(p_idx[:, self.is_left_behind], labels)
+        q_idx_shifted, is_q_in_img = self._shift(q_idx[:, self.is_left_behind], labels)
         diff_right = self.right[list(p_idx_shifted)] - self.right[list(q_idx_shifted)]
-        is_left_under = self.is_left_under.copy()
-        is_left_under[is_left_under] = np.logical_not(is_p_in_img & is_q_in_img)
-        smoothness[is_left_under] = 0
+        is_left_behind = self.is_left_behind.copy()
+        is_left_behind[is_left_behind] = np.logical_not(is_p_in_img & is_q_in_img)
+        smoothness[is_left_behind] = 0
         return smoothness
     
     def add_smooth_weights(self, G, n1, n2, w1, w2, w3, w4):
